e6c014e1ab2e6ab0fd7e14bbe0e56e70ce188173
Tigran Avakyan <tigran.avakyan@icloud.com>
2018-05-24 20:26:48 -0400

    Adding addresses for gelt test

--- contracts/mocks/GeltFixedValidatorSet.sol
+++ contracts/mocks/GeltFixedValidatorSet.sol
@@ -3,7 +3,16 @@ pragma solidity ^0.4.23;
 import "../interfaces/ValidatorSet.sol";
 
 contract GeltFixedValidatorSet {
-  constructor() public {
+  // Current list of addresses entitled to participate in the consensus.
+  address[] public validatorsList;
 
+  constructor() public {
+    validatorsList.push(0x0082a978b3f5962a5b0957d9ee9eef472ee55b42f1);
+    validatorsList.push(0x007d577a597b2742b498cb5cf0c26cdcd726d39e6e);
+  }
+
+  // Called on every block to update node validator list.
+  function getValidators() public constant returns (address[]) {
+      return validatorsList;
   }
 }
