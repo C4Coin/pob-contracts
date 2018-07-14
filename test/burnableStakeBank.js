6656bb2becb0bbe8956eafde2e9da7a6d1307ce0
Jay Butera <buterajay@gmail.com>
2018-07-14 17:49:13 -0400

    Stake by approving

--- test/burnableStakeBank.js
+++ test/burnableStakeBank.js
@@ -1,29 +1,26 @@
 const BurnableStakeBank = artifacts.require('BurnableStakeBank')
+//const StakeBank = artifacts.require('StakeBank');
 const ConsensusToken = artifacts.require('ConsensusToken')
-const Token = artifacts.require('SimpleToken')
 
 contract('Burn dem tokes', ([owner, staker]) => {
   let token, burnBank
 
   beforeEach(async () => {
-    token = await ConsensusToken.new(1000, { from: owner })
+    const tokenCap = 1000
+    token = await ConsensusToken.new(tokenCap, { from: owner })
     burnBank = await BurnableStakeBank.new(token.address)
 
     // Mint tokens
     await token.mint(owner, 1000, { from: owner })
     // Staker gets 100 tokens
-    await token.transfer(staker, 100, { from: owner })
+    //await token.transfer(staker, 100, { from: owner })
   })
 
   it('Stake and burn tokens', async () => {
-    let bal = (await token.balanceOf(staker)).toNumber()
-    assert.equal(bal, 100)
+    await token.approve(burnBank.address, 100, { from: owner })
+    await burnBank.stakeFor(owner, 100, '0x0', { from: owner })
 
-    /*
-      burnBank.stake(100, web3.fromAscii('0x0'), {from: staker})
-      //console.log(await burnBank.totalStaked())
-      let staked = (await burnBank.totalStaked()).toNumber()
-      assert.equal(100, 100)
-      */
+    let staked = (await burnBank.totalStaked()).toNumber()
+    assert.equal(staked, 100)
   })
 })
