b1b9dc337cf459110617757ad07f55406aea9d1a
Jay Butera <buterajay@gmail.com>
2018-07-31 11:42:14 -0500

    18 Generate list of stakers

--- contracts/FtsLib.sol
+++ contracts/FtsLib.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.24;
 
 contract Fts {
+   event Debug(uint256 x);
+
    function _newRandomNumber(bytes32 seed) internal returns (uint256) {
       return uint256( keccak256(seed) );
    }
@@ -9,25 +11,37 @@ contract Fts {
        bytes32 seed,
        address[] stakerIds,
        uint256[] stakerIndices,
-       uint256 totalCoin) public returns (address)
+       uint256 totalCoin,
+       uint256 numBlocks) public returns (address[])
    {
-       require(stakerIds.length == stakerIndices.length);
+       require(stakerIds.length+1 == stakerIndices.length);
+
+       address[] memory selectedStakers = new address[](numBlocks);
 
        // Generate a random index
-       uint256 rndIndex  = _newRandomNumber(seed) % totalCoin;
+       uint256 rndIndex = _newRandomNumber(seed) % totalCoin;
 
-       // Match staker using a binary search
-       uint min = 0;
-       uint max = stakerIndices.length;
-       while (max > min) {
-          uint mid = (max + min + 1) / 2;
-          if (stakerIndices[mid] <= rndIndex) {
-              min = mid;
-          } else {
-              max = mid-1;
+       // Select a staker for each block
+       for (uint256 i = 0; i < numBlocks; i++)
+       {
+          // Generate new random index for subsequent passes
+          if (i != 0) rndIndex = _newRandomNumber( bytes32(rndIndex) ) % totalCoin;
+
+          // Match staker using a binary search
+          uint min = 0;
+          uint max = stakerIndices.length;
+
+          while (max > min) {
+             uint mid = (max + min + 1) / 2;
+             if (stakerIndices[mid] <= rndIndex) {
+                 min = mid;
+             } else {
+                 max = mid-1;
+             }
           }
+          selectedStakers[i] = stakerIds[min];
        }
 
-       return stakerIds[min];
+       return selectedStakers;
    }
 }
--- test/fts.js
+++ test/fts.js
@@ -3,37 +3,26 @@ const FtsLib = artifacts.require('Fts')
 contract('Fts library', accounts => {
   let ftsLib
 
-  it('Should return the second staker from the list', async () => {
+  beforeEach(async () => {
     ftsLib = await FtsLib.new()
+  })
 
+  it('Should return the second staker from the list', async () => {
     // Hash of "00" as a uint256 is 1.8569430475105882587588266137607568536673111973893317399460219858819262702947e+76
-    // That number % totalCoin (651) is 162
-    // Index 162 equates to 2nd account
+    // That number % totalCoin (650) is 197
+    // Index 197 equates to 3rd account
+    // Hash of that hash as uint256 is 7.7325989696766893111127756213298094134926739482120969801295632937101989508141e+76
+    // That number % totalCoin (650) is 162
+    // Index 162 equates to 3rd account
 
     let staker = await ftsLib.fts.call(
       '00',
-      accounts.slice(0, 4),
-      [10, 110, 610, 650],
-      651
+      accounts.slice(0, 4), // Staker address list
+      [0, 10, 110, 610, 650], // Accumulated index list with a 0 prepended
+      650, // Total coins
+      2 // Number of blocks
     )
 
-    assert.equal(staker, accounts[1])
-  })
-
-  it('Should return the first staker from the list', async () => {
-    ftsLib = await FtsLib.new()
-
-    // Hash of "123" as a uint256 is 5.320535884217948059154257054001672881197643928609443669088116914333526164331e+76
-    // That number % totalCoin (651) is 37
-    // Index 37 equates to 1st account
-
-    let staker = await ftsLib.fts.call(
-      '123',
-      accounts.slice(0, 4),
-      [10, 110, 610, 650],
-      651
-    )
-
-    assert.equal(staker, accounts[0])
+    assert.deepEqual(staker, [accounts[2], accounts[2]])
   })
 })
